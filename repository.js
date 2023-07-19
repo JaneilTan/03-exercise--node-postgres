a0e4edc97c702dfd31fa0faf80f41d23128d6167
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-07-19 14:56:26 +1200

    Implemented code to Create, Read, Update and Delete for todos from the database using connection pool

--- repository.js
+++ repository.js
@@ -4,6 +4,8 @@ module.exports = {
   createNewTodo: async (description) => {
     try {
       // TODO: create a todo
+      const newTodo = await pool.query(`INSERT INTO todo (description) VALUES ($1) RETURNING *`, [description]);
+      return(newTodo.rows[0]);
     } catch (error) {
       throw Error(error);
     }
@@ -18,19 +20,24 @@ module.exports = {
       );
       return result.rows;
     } catch (error) {
-      throw Error(error);
+      throw error;
     }
   },
   getTodoById: async (id) => {
     try {
       // TODO: get a todo by id
+      const todo = await pool.query(`SELECT 
+      todo_id AS id, description FROM todo WHERE todo_id = $1`, [id]);
+      return todo.rows[0];
     } catch (error) {
-      throw Error(error);
+      throw new Error(error);
     }
   },
   updateTodoById: async (id, description) => {
     try {
       // TODO: update a todo by id
+      const updatedTodo = await pool.query(`UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *`, [description, id]);
+      return updatedTodo.rows[0];
     } catch (error) {
       throw Error(error);
     }
@@ -38,8 +45,12 @@ module.exports = {
   deleteTodoById: async (id) => {
     try {
       // TODO: delete a todo by id
+      const result = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+      const numberOfDeletedTodos = result.rowCount;
+
+      return numberOfDeletedTodos;
     } catch (error) {
-      throw Error(error);
+      throw new Error(error);
     }
   },
 };
